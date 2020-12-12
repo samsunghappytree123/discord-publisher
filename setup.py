cbe02647bef2d4e3b3d20e242de90c72fcc5fb65
samsunghappytree123 <samsunghappytree123@naver.com>
2020-12-12 12:08:14 +0900

    setup.py 수정

--- setup.py
+++ setup.py
@@ -8,19 +8,5 @@ setup(
     author_email = 'samsunghappytree123@naver.com',
     url = 'https://github.com/samsunghappytree123/discord-publisher',
     install_requires =  ['aiohttp', 'asyncio'],
-    packages = find_packages(exclude = []),
-    keywords = ['discord', 'discord.py', 'discord_publisher', 'discord-publisher'],
-    python_requires = '>=3',
-    package_data = {},
-    zip_safe = False,
-    classifiers         = [
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-    ],
+    keywords = ['discord', 'discord.py', 'discord_publisher', 'discord-publisher']
 )
